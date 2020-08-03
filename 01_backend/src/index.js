16dc74f660f8819702fff61e9aa88d94e1ca5451
brunosana <brunosa@dcomp.ufs.br>
2020-08-03 18:19:15 -0300

    Building a API REST with Express and Middlewares

--- 01_backend/src/index.js
+++ 01_backend/src/index.js
@@ -1,7 +1,20 @@
 const express = require('express');
+const {uuid} = require('uuidv4');
 const { request, response } = require('express');
 
+const projects = [];
+
 const app = express();
+
+function logRequests(request, response, next){
+    const { method, url } = request;
+    const label = `[${method.toUpperCase()}] - ${url}`;
+    console.log(label);
+    next();
+}
+
+app.use(logRequests);
+
 app.use(express.json())
 
 app.get('/', (request, response) =>{
@@ -9,29 +22,62 @@ app.get('/', (request, response) =>{
 });
 
 app.get('/projects', (request, response)=>{
-    return response.json([
-        "elemento1",
-        "elemento2",
-        "elemento3",
-        "elemento4"
-    ]);
+
+    const {  name } = request.query;
+
+    const results = name
+    ? projects.filter(p => p.name.includes(name)) : projects
+
+    return response.json(results);
 });
 
 app.post('/projects', (request, response) => {
-    const body = request.body;
-    return response.json({
-        name: body.name,
-        desc: body.desc
-    });
+    const {name, desc} = request.body;
+    const project = {id: uuid(), name, desc};
+    projects.push(project);
+    return response.json(project);
 });
 
 app.put('/projects/:id', (request, response) => {
-    const params = request.params;
-    const query = request.query;
-    return response.json({
-        id_passed: params.id,
-        queryParams: query
-    })
+    const {id} = request.params;
+    const projectIndex = projects.findIndex(p => p.id = id);
+    console.log("index", projectIndex);
+    const {name, desc} = request.body;
+    
+    if (projectIndex < 0){
+        return response.status(400).json({message: "Project not Found"});
+    }
+
+    const project = {
+        id,
+        name,
+        desc
+    }
+
+    projects[projectIndex] = project;
+
+    return response.json(project)
+});
+
+app.get('/projects/:id', (request, response) => {
+    const {id} = request.params;
+    const projectIndex = projects.findIndex(p => p.id = id);
+    
+    if (projectIndex < 0){
+        return response.status(400).json({message: "Project not Found"});
+    }
+    const project = projects[projectIndex];
+    return response.json(project)
+});
+
+app.delete('/projects/:id', (request, response)=> {
+    const { id } = request.params;
+    const projectIndex = projects.findIndex(p => p.id = id);
+    if(projectIndex < 0){
+        return request.status(400).json({message: "Project not found"});
+    }
+    projects.splice(projectIndex, 1);
+    return response.status(204).send()
 });
 
 app.listen(3333, ()=>{
